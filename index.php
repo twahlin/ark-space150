0c99489aca3e7036a92d3284a815e2fa2c3fa493
Tom Wahlin <tom.wahlin@gmail.com>
2012-02-06 15:59:47 -0600

    Initial commits and updates

--- index.php
+++ index.php
@@ -6,46 +6,210 @@
     <link rel="stylesheet" href="public/styles/style.css" type="text/css" />
     <link rel="Shortcut Icon" href="public/images/favicon.ico" type="image/x-icon" />
 
-    <script src="public/scripts/jquery-1.6.1.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" type="text/javascript" charset="utf-8"></script>
     <script src="public/scripts/scripts.js" type="text/javascript" charset="utf-8"></script>	
+    
+    
+    <script>
+  	$(function() {
+  		$( "#tabs" ).tabs();
+  	});
+  	</script>
+    
 </head>
 
 <body>
+  <div class="wrapper">
 
   <?php
   require_once 'public/scripts/magpie/rss_fetch.inc';
-
-  $url = 'http://feeds.feedburner.com/railscasts';
-  $rss = fetch_rss($url);
-
-  echo "Site: ", $rss->channel['title'], "<br>
-  ";
-  foreach ($rss->items as $item ) {
-  	$title = $item[title];
-  	$url   = $item[link];
-  	echo "<a href=$url>$title</a></li><br>
-  ";
-  }
   ?>
-
-  Other stuffs
 
   <?php
 
   $url = 'http://ws.audioscrobbler.com/1.0/user/twahlin/recenttracks.rss';
   $rss = fetch_rss($url);
 
-  echo "Site: ", $rss->channel['title'], "<br>
-  ";
   $i = 0; //yo
   foreach ($rss->items as $item ) {
   	$title = $item[title];
   	$url   = $item[link];
-  	echo "<a href=$url>$title</a></li><br>
-  ";
+  	echo "<p>$title</p>";
   if (++$i == 2) break; //yo
   }
   ?>
+  
+  
+  <?php
+
+  $url = 'http://dribbble.com/twahlin/shots.rss';
+  $rss = fetch_rss($url);
+
+  $i = 0; //yo
+  foreach ($rss->items as $item ) {
+  	$title = $item[title];
+  	$description = $item[description];
+  	echo "<p>$title</p><p>$description</p>";
+  if (++$i == 2) break; //yo
+  }
+  ?>
+  
+  <?php
+
+  $url = 'http://dribbble.com/billyfrench/shots.rss';
+  $rss = fetch_rss($url);
+
+  $i = 0; //yo
+  foreach ($rss->items as $item ) {
+  	$title = $item[title];
+  	$description = $item[description];
+  	echo "<p>$title</p><p>$description</p>";
+  if (++$i == 2) break; //yo
+  }
+  ?>
+  
+  
+  <div class="pixels"></div>
+  
+  
+  <div class="video_room">
+    <li><iframe src="http://player.vimeo.com/video/21965821?title=0&amp;byline=0&amp;portrait=0" width="400" height="225" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe><p><a href="http://vimeo.com/21965821">Tycho live visuals test footage</a> from <a href="http://vimeo.com/iso50">ISO50</a> on <a href="http://vimeo.com">Vimeo</a>.</p></li>
+    <li><iframe src="http://player.vimeo.com/video/36167539?color=ff9933" width="400" height="225" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe><p><a href="http://vimeo.com/36167539">M.I.A, Bad Girls</a> from <a href="http://vimeo.com/gavras">ROMAIN-GAVRAS</a> on <a href="http://vimeo.com">Vimeo</a>.</p></li>
+  </div>  
+  
+  
+  
+<div class="tab_test">
+  <ul>
+    <li>Tab Nav 1</li>
+  </ul>
+</div>
+  
+  
+  <div class="shadow-test"><div class="room-light"></div></div>
+  <div class="light-switch light-switch-off"></div>
+
+
+<div class="album-art">
+  <div class="album-art-img"></div>
+</div>
+
+
+</div>
+
+<script>
+$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+  {
+    tags: "bulldog",
+    tagmode: "any",
+    format: "json"
+  },
+  function(data) {
+    $.each(data.items, function(i,item){
+      $("<img/>").attr("src", item.media.m).appendTo("#images");
+      if ( i == 3 ) return false;
+    });
+  });</script>
+    <div id="images"></div>
+    
+    
+    
+    
+    
+    
+    
+    <script>
+    
+    
+    
+    $(document).ready(function() {
+      // Declare variables to hold twitter API url and user name
+      var twitter_api_url = 'http://search.twitter.com/search.json';
+      var twitter_user    = 'twahlin';
+
+      // Enable caching
+      $.ajaxSetup({ cache: true });
+
+      // Send JSON request
+      // The returned JSON object will have a property called "results" where we find
+      // a list of the tweets matching our request query
+      $.getJSON(
+        twitter_api_url + '?callback=?&rpp=5&q=from:' + twitter_user,
+        function(data) {
+          $.each(data.results, function(i, tweet) {
+            // Uncomment line below to show tweet data in Fire Bug console
+            // Very helpful to find out what is available in the tweet objects
+            //console.log(tweet);
+
+            // Before we continue we check that we got data
+            if(tweet.text !== undefined) {
+              // Calculate how many hours ago was the tweet posted
+              var date_tweet = new Date(tweet.created_at);
+              var date_now   = new Date();
+              var date_diff  = date_now - date_tweet;
+              var hours      = Math.round(date_diff/(1000*60*60));
+
+              // Build the html string for the current tweet
+              var tweet_html = '<div class="tweet_text">';
+              tweet_html    += '<a href="http://www.twitter.com/';
+              tweet_html    += twitter_user + '/status/' + tweet.id + '">';
+              tweet_html    += tweet.text + '<\/a><\/div>';
+              tweet_html    += '<div class="tweet_hours">' + hours;
+              tweet_html    += ' hours ago<\/div>';
+
+              // Append html string to tweet_container div
+              $('#tweet_container').append(tweet_html);
+            }
+          });
+        }
+      );
+    });
+  </script>
+      <div id="tweet_container"></div>
+      
+      
+      
+      
+      
+      
+      <script>
+      
+      var username='twahlin'; // set user name
+      var format='json'; // set format, you really don't have an option on this one
+      var url='http://api.twitter.com/1/statuses/user_timeline/'+username+'.'+format+'?callback=?'; // make the url
+
+      	$.getJSON(url,function(tweet){ // get the tweets
+      		$("#last-tweet").html(tweet[0].text); // get the first tweet in the response and place it inside the div
+      	});
+      	
+      </script>
+      <div id="last-tweet"></div>
+      
+      
+      
+      
+      <script>
+      
+      var arr = [ "one", "two", "three", "four", "five" ];
+      
+       jQuery.each(arr, function() {
+         $("#" + this).text("Mine is " + this + ".");
+          return (this != "three"); // will stop running after "three"
+      });
+      
+      </script>
+    
+    
+    
+    
+
+
+
+
+
+
+
 
 </body>
 </html>
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,3 +1,279 @@
 $(document).ready(function() {
-  // put all your jQuery goodness in here.
+
+  $('.light-switch').click(function() {
+    $(this).toggleClass('light-switch-off');
+    $('.room-light').fadeToggle();
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
 });
+
+
+
+$('#target').click(function() {
+  alert('Handler for .click() called.');
+});
+
+
+
+$(window).scroll(function() {
+    // use the value from $(window).scrollTop();
+    var pixels_scrolled = $(window).scrollTop();
+    console.log(pixels_scrolled); 
+		$('.pixels').html(pixels_scrolled); 
+
+		
+		$("#waves_1").css("background-position", pixels_scrolled * 1);
+		$("#waves_2").css("background-position", pixels_scrolled * 8);
+		$("#waves_3").css("background-position", pixels_scrolled * 10);
+		$("#waves_4").css("background-position", pixels_scrolled * 6);
+		$("#waves_5").css("background-position", pixels_scrolled * 4);
+		$("#waves_6").css("background-position", pixels_scrolled * 12);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Plugin: jquery.zLastFM
+ * 
+ * Version: 1.0.0
+ * (c) Copyright 2010, Zazar Ltd
+ * 
+ * Description: jQuery plugin for display of Yahoo! Weather feeds
+ * 
+ * History:
+ *
+ **/
+
+(function($){
+
+	$.fn.lastfm = function(username, display, apikey, options) {	
+	
+		// Set pluign defaults
+		var defaults = {
+			limit: 5,	
+			header: true,
+			title: '',
+			imagesize: 'medium',
+			image: true,
+			noimage: '',
+			name: true,
+			artist: true,
+			playcount: true,
+			showerror: true
+		};  
+		var options = $.extend(defaults, options); 
+		
+		// Functions
+		return this.each(function(i, e) {
+			var $e = $(e);
+			
+			// Add feed class to user div
+			if (!$e.hasClass('lastFM')) $e.addClass('lastFM');
+
+			// Get API method
+			if (display == 'lovedtracks') {
+				options.title = 'Loved Tracks';
+				var method = 'user.getLovedTracks';
+
+			} else if (display == 'recenttracks') {
+				options.title = 'Recent Tracks';
+				var method = 'user.getRecentTracks';
+
+			} else if (display == 'topalbums') {
+				options.title = 'Top Albums';
+				var method = 'user.getTopAlbums';
+
+			} else if (display == 'topartists') {
+				options.title = 'Top Artists';
+				var method = 'user.getTopArtists';
+
+			} else if (display == 'toptracks') {
+				options.title = 'Top Tracks';
+				var method = 'user.getTopTracks';
+
+			} else {
+				if (options.showerror)  $e.html('<p>LastFM feed invalid</p>');
+				return false;
+			}
+
+			// Create LastFM API address
+			var api = 'http://ws.audioscrobbler.com/2.0/?method='+ method +'&user='+ username +'&api_key='+ apikey +'&limit='+ options.limit +'&format=json&callback=?';
+
+			// Send request
+			$.ajax({
+				type: 'GET',
+				url: api,
+				dataType: 'json',
+				success: function(data) {
+
+					if (data) {
+
+						// Process data depending on feed
+						if (display == 'lovedtracks') {
+							
+							_callback(e, display, data.lovedtracks.track, options);
+
+						} else if (display == 'recenttracks') {
+						
+							_callback(e, display, data.recenttracks.track, options);
+
+						} else if (display == 'topalbums') {
+
+							_callback(e, display, data.topalbums.album, options);
+
+						} else if (display == 'topartists') {
+		
+							_callback(e, display, data.topartists.artist, options);
+
+						} else if (display == 'toptracks') {
+
+							_callback(e, display, data.toptracks.track, options);
+						}
+
+					} else {
+						if (options.showerror) $e.html('<p>LastFM information unavailable</p>');
+					}
+				},
+				error: function(data) {
+					if (options.showerror)  $e.html('<p>LastFM request failed</p>');
+				}
+			});
+
+		});
+	};
+
+	// Function to get feed items
+	var _callback = function(e, display, feeds, options) {
+		var $e = $(e);
+		var row = 'odd';
+		var html = '';
+
+		// Add header if required
+		html += '<div class="lastFMHeader">'+ options.title + '</div>';
+
+		// Add feed container
+		html += '<div class="lastFMBody '+ display +'">';
+			
+		if (feeds) {
+
+			html += '<ul>';
+
+			var count = feeds.length;
+			if (count > options.limit) count = options.limit;
+			
+			// Add feeds
+			for (var i=0; i<count; i++) {
+
+				// Get individual feed
+				var item = feeds[i];				
+
+				
+				
+			 	// Get feed data		
+				var name = item.name;		
+				var url = _getValidURL(item.url)
+				var artist = '';
+				var imgurl = '';
+				var playcount = null;
+				
+				html += '<li class="itemRow '+ row +'"><a target="_blank" title="Listen to '+ item.name +' on Last.FM" rel="external" href="'+ url +'">';
+
+				if (display == 'lovedtracks' || display == 'topalbums' || display == 'toptracks') {
+
+					artist = '<a href="'+ _getValidURL(item.artist.url) +'" title="More about '+ item.artist.name +' on Last.FM">'+ item.artist.name +'</a>';
+				} else if (display == 'recenttracks') {
+			
+					artist = item.artist['#text'];
+				}
+						
+				if (display == 'topalbums' || display == 'topartists' || display == 'toptracks') playcount = item.playcount;
+
+				// Add image
+				if (options.image) {
+
+					// Check for feed image url
+					if (item.image) {
+
+						// Get image index
+						if (options.imagesize == 'small') {
+							var imgindex = 0;
+		
+						} else if (options.imagesize == 'medium') {
+							var imgindex = 1;
+	
+						} else if (options.imagesize == 'large') {
+							var imgindex = 2;
+	
+						} else if (options.imagesize == 'extralarge') {
+							var imgindex = 3;
+						}	
+						
+						// Get image url or default
+						imgurl = _getValidURL(item.image[imgindex]['#text']);
+					}
+
+					// Apply default image if requried
+					if (imgurl == '') imgurl = options.noimage;
+					if (imgurl != '') html += '<div class="artWrap" style="background-image: url('+ imgurl +')"><img src="'+ imgurl +'" alt="'+ item.name +'" /></div>'
+				}
+
+				// Add name
+				if (options.name) html += '<div class="track_info"><p><em class="itemName">'+ item.name +'</em><br />'
+
+				// Add artist
+				if (options.artist) html += '<em class="itemArtist">'+ artist +'</em></p></div>';
+
+				// Add play count
+				if (options.playcount && playcount != null) html += '<div class="itemPlaycount">'+ playcount +' plays</div>'
+
+				html += '</a></li>';
+
+				// Alternate row classes
+				if (row == 'odd') { row = 'even'; } else { row = 'odd'; }
+			}
+
+			html += '</ul>';
+			
+		} else {
+			html += '<p>No items to display</p>';e
+		};
+
+
+		html += '</div>';
+		
+		$e.append(html);
+	};
+	
+	// Ensure URL is formatted correctly
+	var _getValidURL = function(u) {
+
+		var url = u;
+		if (u != '' && u.substr(0,7) != 'http://') url = 'http://'+ u;
+
+		return url;
+	};
+})(jQuery);
+
+
+
